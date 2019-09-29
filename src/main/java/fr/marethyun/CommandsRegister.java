8091f11d9cbe28979ddaa9d1e47ad9b0ff6cd846
marethyun <bacci.ange@gmail.com>
2019-09-29 12:26:11 +0200

    Documented message and command processing classes

--- src/main/java/fr/marethyun/CommandsRegister.java
+++ src/main/java/fr/marethyun/CommandsRegister.java
@@ -8,26 +8,67 @@ import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
+/**
+ * The commands register
+ *
+ * This class is responsible for the handling of commands fired by the user in the provided channel and guild.
+ */
 public final class CommandsRegister implements MessageProcessor {
 
+    /**
+     * The class' logger
+     */
     private static final Logger LOGGER = Logger.getLogger(CommandsRegister.class.getName());
 
+    /**
+     * The commands prefix
+     */
     private char prefix;
+
+    /**
+     * The set the built command objects
+     */
     private ArrayList<Command> commands = new ArrayList<>();
 
+    /**
+     * The class' constructor
+     *
+     * @param prefix The commands prefix
+     */
     public CommandsRegister(char prefix) {
         this.prefix = prefix;
     }
 
+    /**
+     * Registers a command from a callback and aliases.
+     *
+     * A command can be referenced by multiple aliases.
+     *
+     * @param executor The callback
+     * @param aliases The command aliases
+     */
     public void registerCommand(CommandExecutor executor, String... aliases) {
         if (aliases.length < 1) throw new CommandException("A command must at least have one alias");
         this.commands.add(new Command(executor, new ArrayList<>(Arrays.asList(aliases))));
     }
 
+    /**
+     * Registers a command with only one alias, its name.
+     *
+     * @see CommandsRegister#registerCommand(CommandExecutor, String...)
+     *
+     * @param executor The callback
+     * @param alias The command alias
+     */
     public void registerCommand(CommandExecutor executor, String alias) {
         registerCommand(executor, new String[]{alias});
     }
 
+    /**
+     * Process a command from a discord message
+     *
+     * @param message The message
+     */
     @Override
     public void processMessage(Message message) {
 
@@ -71,35 +112,88 @@ public final class CommandsRegister implements MessageProcessor {
         }
     }
 
+    /**
+     * Getter for the commands
+     * @return the commands
+     */
     public ArrayList<Command> getCommands() {
         return commands;
     }
 
+    /**
+     * Getter for the prefix
+     * @return the prefix
+     */
     public char getPrefix() {
         return prefix;
     }
 
+    /**
+     * The callback interface.
+     */
     public interface CommandExecutor {
+        /**
+         * Executes the command
+         *
+         * @param message The discord message
+         * @param mention The sender's mention string
+         * @throws CommandException if something went wrong
+         */
         void execute(Message message, String mention) throws CommandException;
     }
 
-    public class Command {
+    /**
+     * A POJO that represents a command.
+     *
+     * Wraps a callback and aliases of the command.
+     */
+    public class Command implements CommandExecutor {
+        /**
+         * The callback
+         */
         private CommandExecutor executor;
+
+        /**
+         * The aliases
+         */
         private ArrayList<String> aliases;
 
+        /**
+         * The constructor
+         *
+         * @param executor The callback
+         * @param aliases The aliases
+         */
         public Command(CommandExecutor executor, ArrayList<String> aliases) {
             this.executor = executor;
             this.aliases = aliases;
         }
 
+        /**
+         * Just executes the wrapped callback
+         *
+         * @param message The discord message
+         * @param mention The sender's mention string
+         */
+        @Override
         public void execute(Message message, String mention) {
             this.executor.execute(message, mention);
         }
 
+        /**
+         * The getter for the callback
+         *
+         * @return the callback
+         */
         public CommandExecutor getExecutor() {
             return executor;
         }
 
+        /**
+         * The getter for the aliases
+         *
+         * @return the aliases
+         */
         public ArrayList<String> getAliases() {
             return aliases;
         }
--- src/main/java/fr/marethyun/MessageProcessor.java
+++ src/main/java/fr/marethyun/MessageProcessor.java
@@ -2,6 +2,17 @@ package fr.marethyun;
 
 import discord4j.core.object.entity.Message;
 
+/**
+ * Just provides a method to process a discord message
+ *
+ * @see CommandsRegister
+ * @see GroupAllocator
+ */
 public interface MessageProcessor {
+    /**
+     * Processes a message
+     *
+     * @param message The message
+     */
     void processMessage(Message message);
 }
